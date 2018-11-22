80f074a22059a71e7b1babd346e170d662d2f6af
amrabdelhamidhassan <33031176+amrabdelhamidhassan@users.noreply.github.com>
2018-11-22 16:06:31 +0200

    Add files via upload

---
+++ NetworksAssignment1/generator.py
@@ -0,0 +1,39 @@
+def xor(a, b): 
+    result = "" 
+    for i in range(1, len(b)): 
+        if a[i] == b[i]: 
+            result+='0' 
+        else: 
+            result+='1'   
+    return result 
+f=open("messagegenerator.txt","r")
+if f.mode=='r' :
+       inputtext=f.read()
+realmessage=inputtext.split()[0]
+generator=inputtext.split()[1]
+r=len(generator)
+message=realmessage+(r-1)*'0'
+m=len(message)
+counter=r
+buffer=message[0 :counter]
+while counter < m: 
+   
+    if buffer[0] == '1': 
+            buffer = xor(generator, buffer) + message[counter] 
+   
+    else:  
+        buffer = xor('0'*counter, buffer) + message[counter] 
+    counter += 1
+    
+if buffer[0] == '1': 
+    buffer = xor(generator, buffer) 
+else: 
+    buffer = xor('0'*counter, buffer)
+realmessage+=buffer
+f=open("transmittedmessage.txt","w")
+f.write(realmessage)
+f.write("\n")
+f.write(generator)
+f.close()
+print("The transmitted message and the genrator are outputed in transmittedmessage.txt file to be inputted to the verifier and alter")
+input("press any button to continue")
